15eebc6abbec3c61ea49c9dcb48c60158d657c1a
Vilar95 <e.vilar95@gmail.com>
2024-05-15 18:36:32 -0300

    Update

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_webapi_first_course/models/journal.dart';
+import 'package:flutter_webapi_first_course/screens/home_screen/add_journal_screen/add_journal_screen.dart';
 import 'package:flutter_webapi_first_course/services/journal_service.dart';
 import 'package:google_fonts/google_fonts.dart';
 
@@ -8,8 +10,9 @@ void main() {
   runApp(const MyApp());
 
   JournalService service = JournalService();
-  service.register("Olá mundo!");
-  service.get();
+  service.register(Journal.empty());
+  //service.register("Olá mundo!");
+  //service.get();
 }
 
 class MyApp extends StatelessWidget {
@@ -27,14 +30,26 @@ class MyApp extends StatelessWidget {
           titleTextStyle: TextStyle(
             color: Colors.white,
           ),
+          actionsIconTheme: IconThemeData(color: Colors.white),
+          iconTheme: IconThemeData(color: Colors.white),
         ),
-      textTheme: GoogleFonts.bitterTextTheme(),
+        textTheme: GoogleFonts.bitterTextTheme(),
       ),
       darkTheme: ThemeData.dark(),
       themeMode: ThemeMode.light,
       initialRoute: "home",
       routes: {
         "home": (context) => const HomeScreen(),
+      },
+      onGenerateRoute: (settings) {
+        if (settings.name == 'add-journal') {
+          final Journal journal = settings.arguments as Journal;
+          return MaterialPageRoute(
+            builder: (context) {
+              return AddJournalScreen(journal: journal);
+            },
+          );
+        }
       },
     );
   }
--- lib/services/journal_service.dart
+++ lib/services/journal_service.dart
@@ -1,24 +1,33 @@
 import 'dart:convert';
-import 'package:flutter_webapi_first_course/services/http_interceptors';
+import 'package:flutter_webapi_first_course/models/journal.dart';
+import 'package:flutter_webapi_first_course/services/http_interceptors.dart';
 import 'package:http/http.dart' as http;
 import 'package:http_interceptor/http/http.dart';
 
 class JournalService {
   static const String url = "http://localhost:3000/";
-  static const String resourse = "learnhttp/";
+  static const String resourse = "journals/";
 
-  http.Client client = InterceptedClient.build(interceptors: [LoggingInterceptor()]);
+  http.Client client =
+      InterceptedClient.build(interceptors: [LoggingInterceptor()]);
 
   String getUrl() {
     return "$url$resourse";
   }
 
-  register(String content) {
-    client.post(
+  Future<bool> register(Journal journal) async {
+    String jsonJournal = json.encode(journal.toMap());
+    http.Response response = await client.post(
       Uri.parse(getUrl()),
-      body: json.encode({"content": content}),
-      headers: {"Content-Type": "application/json; charset=UTF-8"},
+      headers: {
+        "Content-Type": "application/json;",
+      },
+      body: jsonJournal,
     );
+    if (response.statusCode == 201) {
+      return true;
+    }
+    return false;
   }
 
   Future<String> get() async {
